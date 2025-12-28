ca02d9aa3f4b0234cb4577c2ab74bc63a10d6c34
MRMutahir <mutahirkarem820@gmail.com>
2025-12-28 22:09:32 +0500

    dabase added

---
+++ index.js
@@ -0,0 +1,71 @@
+const dialogflow = require("@google-cloud/dialogflow");
+const { WebhookClient, Payload, Button } = require("dialogflow-fulfillment");
+const express = require("express");
+const nodemailer = require("nodemailer");
+const { createClient } = require('@supabase/supabase-js');
+const cors = require("cors");
+const supabase = createClient('Supabase URL', 'Supabase Anon Key');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.post("/webhook", async (req, res) => {
+  var id = res.req.body.session.substr(43);
+  console.log(id);
+  const agent = new WebhookClient({ request: req, response: res });
+
+  function hi(agent) {
+    console.log(`intent  =>  hi`);
+    agent.add(
+      "Hi I am the AI assistant of dialogflow, You can also connect with human directly"
+    );
+
+  }
+
+  async function booking(agent) {
+    const { destination, departure, date, number, email, phone } = agent.parameters;
+
+    const { data, error } = await supabase
+      .from('bookings')
+      .insert([
+        {
+          destination: destination,
+          departure: departure,
+          travel_date: date,
+          number_of_people: number,
+          email: email,
+          phone: phone
+        },
+      ]);
+
+    if (error) {
+      console.error('Error inserting into Supabase:', error);
+    } else {
+      console.log('Successfully saved to Supabase:', data);
+    }
+
+    agent.add(
+      `Hi there, Your booking has been registered for ${destination} from ${departure} on ${date} for ${number} person. We sent you email at ${email} and sent a message on your number ${phone}`
+    );
+
+    console.log("Number of People", number);
+    console.log("User Email", email);
+    console.log("User Phone Number:", phone);
+  }
+
+  let intentMap = new Map();
+  intentMap.set("Default Welcome Intent", hi);
+  intentMap.set("reservation", booking);
+  agent.handleRequest(intentMap);
+});
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
